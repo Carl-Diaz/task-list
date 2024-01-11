10a153068dad0b45d1eb97af0f46dfb7a7cd0bad
alberto diaz <albersan338@gmail.com>
2024-01-11 15:26:57 -0500

    implentacion de chakra

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
+// index.js o index.jsx
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App"; // Asegúrate de ajustar la ruta a tu componente principal
+import theme from "./theme"; // Importa tu archivo de tema personalizado
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
---
+++ src/theme.jsx
@@ -0,0 +1,8 @@
+// theme.js
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  // Configuración del tema (opcional)
+});
+
+export default theme;
